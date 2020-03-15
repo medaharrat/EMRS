6006a70e2bce847981464ef93bee625da7012f73
AbdoElfathi <abdessamadelfathi2018@gmail.com>
2020-03-15 01:56:44 +0100

    just added happy testing image

--- EMRS/views.py
+++ EMRS/views.py
@@ -6,10 +6,11 @@ from keras.preprocessing.image import img_to_array
 from keras.preprocessing       import image
 import cv2
 import numpy as np
-#Imports for webscaping
-from bs4 import BeautifulSoup as SOUP 
-import re 
-import requests as HTTP 
+
+# imports for WebScraping
+from bs4 import BeautifulSoup
+import urllib.request
+
 
 # Create your views here.
 def Home(request):  
@@ -19,23 +20,38 @@ def Capture(request):
     return render(request,'capture.html') 
 
 def GetMovies(request, mood):
-    movies = ["movviiiie"]
-   
+    category = GetMatchGenre(mood)
+    movies = {}
+    filmCounter = 0
+
+    # get the html page of every category
+    html_page = urllib.request.urlopen("https://www.imdb.com/search/title/?title_type=tv_series,tv_miniseries&num_votes=5000,&genres={}&sort=user_rating,desc&start=1&ref_=adv_nxt".format(category))
+    soup = BeautifulSoup(html_page, "lxml")
+
+    # filling the movies into movie list
+    for img in zip(soup.findAll("img", class_="loadlate"),soup.findAll("div", class_="lister-item-content"), soup.findAll("span", class_="lister-item-year text-muted unbold"), soup.findAll("div", class_="inline-block ratings-imdb-rating")):
+
+        movies[filmCounter] = {"name" : img[0]['alt'],"released_date" : img[2].text, "rating" : img[3].find("strong").text,"image_url" :img[0]['loadlate'], "description" : img[1].findChildren('p')[1].text, }
+
+        filmCounter += 1
+        
+        
+
    # OUR SCRAPER GOES HERE
 
     return render(request,'movies.html', {'mood':mood, 'movies':movies})
 
 def GetMatchGenre(emotion):
     if(emotion == "Sad"): 
-        genre = 'drama'
+        genre = 'Drama'
     elif(emotion == "Happy"): 
-        genre = 'musical'
+        genre = 'Musical'
     elif(emotion == "Angry"): 
-        genre = 'family'
+        genre = 'Comedy'
     elif(emotion == "Surprise"): 
-        genre = 'film_noir'
+        genre = 'Fantasy'
     elif(emotion == "Neutral"): 
-        genre = 'western'
+        genre = 'Western'
     return genre
 
 def Seen(request):
@@ -48,7 +64,7 @@ def HandlePicRequest(request):
 
     labels = []
 
-    img = cv2.imread("./EMRS/static/img/face5.jpg")
+    img = cv2.imread("./EMRS/static/img/happy.jpg")
     
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
